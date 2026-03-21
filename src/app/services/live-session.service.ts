6f96081f5009e41424f26ebd1743b40c77e4addb
Aadhithya J <aadhithyaj0903@gmail.com>
2026-03-21 07:50:53 +0530

    api url to environment

--- src/app/services/live-session.service.ts
+++ src/app/services/live-session.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
-const BASE = 'http://localhost:5195/api/livesession';
+const BASE = environment.apiUrl + '/live-sessions';
 
 export interface LiveSessionDto {
   id: string;
