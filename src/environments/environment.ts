a3c05a1e72a0d743ac93797e726b5e4e12b6f93e
Karthikeyan <karthikeyanvg19@gmail.com>
2026-03-27 09:15:32 +0530

    fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiUrl: 'http://localhost:5195/api',
-  // apiUrl: 'https://api.b2pteachers.com/api',
+  // apiUrl: 'http://localhost:5195/api',
+  apiUrl: 'https://api.b2pteachers.com/api',
   razorpayKey: 'rzp_test_STV98WrDHNwOZe'
 };
